54c98ac6da73a298aa1473e87b08aede0d937b95
John <lzy2099@126.com>
2020-07-15 09:55:54 +0800

    下班啦

--- another_blog/article/views.py
+++ another_blog/article/views.py
@@ -7,3 +7,9 @@ def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    context = {'article': article}
+    return render(request, 'article/detail.html', context)
