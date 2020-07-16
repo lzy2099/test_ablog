e9bc1f5696baa2b88abd9e310cd6401be6a5b22a
John <lzy2099@126.com>
2020-07-16 12:02:04 +0800

    增加 注册删除用户功能

--- another_blog/userprofile/forms.py
+++ another_blog/userprofile/forms.py
@@ -2,6 +2,24 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+# 登录表单
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+
+# 新用户注册表单
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+        def clean_password2(self):
+            data = self.cleaned_data
+            if data.get('password') == data.get('password2'):
+                return data.get('password')
+            else:
+                raise forms.ValidationError('两次密码输入不一致，请确认后重试。')
